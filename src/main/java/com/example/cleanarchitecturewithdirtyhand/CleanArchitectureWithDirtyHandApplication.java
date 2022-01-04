83889257e79f76252672328c92707cef08906a5d
poppo <poppo@woowahan.com>
2022-01-04 13:43:41 +0900

    [chore] project init

---
+++ src/main/java/com/example/cleanarchitecturewithdirtyhand/CleanArchitectureWithDirtyHandApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecturewithdirtyhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureWithDirtyHandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureWithDirtyHandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cleanarchitecturewithdirtyhand/CleanArchitectureWithDirtyHandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecturewithdirtyhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureWithDirtyHandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
